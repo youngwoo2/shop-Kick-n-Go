c2acdc223f91f8489918f76ea7cf1cafc17b5ca0
seongyeongit <wjd12118@nate.com>
2024-06-25 09:42:05 +0900

    Feat: 통계 일별/주별/월별/특정기간 & 오늘의 매출 구현중... #1 #3 #4 #5 #6

---
+++ src/main/java/com/sh/shop_kick_n_go/total_order/model/dto/TotalOrderDto.java
@@ -0,0 +1,20 @@
+package com.sh.shop_kick_n_go.total_order.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TotalOrderDto {
+    private int orderId;
+    private int userId;
+    private String ordererName;
+    private String ordererAddress;
+    private LocalDateTime orderDate;
+    private String status;
+    private int totalPrice;
+}
---
+++ src/main/java/com/sh/shop_kick_n_go/total_order/model/service/TotalOrderService.java
@@ -0,0 +1,37 @@
+package com.sh.shop_kick_n_go.total_order.model.service;
+
+import com.sh.shop_kick_n_go.total_order.model.dao.TotalOrderMapper;
+import com.sh.shop_kick_n_go.total_order.model.dto.TotalOrderDto;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class TotalOrderService {
+    private final TotalOrderMapper totalOrderMapper;
+
+    public TotalOrderService(TotalOrderMapper totalOrderMapper) {
+        this.totalOrderMapper = totalOrderMapper;
+    }
+
+    public int getTodaySales() {
+        Integer result = totalOrderMapper.getTodaySales();
+        return result != null ? result : 0;
+    }
+
+    public List<TotalOrderDto> getDailySales(LocalDate startDate, LocalDate endDate) {
+        return totalOrderMapper.getDailySales(startDate, endDate);
+    }
+
+    public List<TotalOrderDto> getWeeklySales(LocalDate startDate, LocalDate endDate) {
+        return totalOrderMapper.getWeeklySales(startDate, endDate);
+    }
+
+    public List<TotalOrderDto> getMonthlySales(LocalDate startDate, LocalDate endDate) {
+        return totalOrderMapper.getMonthlySales(startDate, endDate);
+    }
+
+    public List<TotalOrderDto> getPeriodSales(LocalDate startDate, LocalDate endDate) {
+        return totalOrderMapper.getPeriodSales(startDate, endDate);
+    }
+}
