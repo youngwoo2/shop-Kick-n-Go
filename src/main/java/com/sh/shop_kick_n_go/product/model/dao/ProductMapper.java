44ab6c4a93c7c6f04e0c505c231a8d8a58d3f9aa
jeongeun1 <162237367+jeongeun1@users.noreply.github.com>
2024-06-25 18:12:51 +0900

    Feat: 상품수정 #37

--- src/main/java/com/sh/shop_kick_n_go/product/model/dao/ProductMapper.java
+++ src/main/java/com/sh/shop_kick_n_go/product/model/dao/ProductMapper.java
@@ -1,17 +1,19 @@
 package com.sh.shop_kick_n_go.product.model.dao;
 
-import com.sh.shop_kick_n_go.product.model.dto.CategoryDto2;
 import com.sh.shop_kick_n_go.product.model.dto.ProductDto;
+import com.sh.shop_kick_n_go.product.model.dto.ProductRegistDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface ProductMapper {
+    // 상품조회
     List<ProductDto> findAll();
-
-    int insertProduct();
-    List<CategoryDto2> findAllCategory();
-
-    int updateProduct();
+    // 상품등록
+    int insertProduct(ProductDto productDto);
+    // 상품수정
+    int updateProduct(ProductDto productDto);
+    // 상품삭제
+    List<ProductDto> deleteChecked();
 }
--- src/main/java/com/sh/shop_kick_n_go/product/model/service/ProductCommandService.java
+++ src/main/java/com/sh/shop_kick_n_go/product/model/service/ProductCommandService.java
@@ -2,9 +2,12 @@ package com.sh.shop_kick_n_go.product.model.service;
 
 import com.sh.shop_kick_n_go.product.model.dao.ProductMapper;
 import com.sh.shop_kick_n_go.product.model.dto.ProductDto;
+import com.sh.shop_kick_n_go.product.model.dto.ProductRegistDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +16,14 @@ public class ProductCommandService {
     private final ProductMapper productMapper;
 
     public int insertProduct(ProductDto productDto) {
-        return productMapper.insertProduct();
+        return productMapper.insertProduct(productDto);
     }
 
     public int updateProduct(ProductDto productDto) {
-        return productMapper.updateProduct();
+        return productMapper.updateProduct(productDto);
+    }
+
+    public List<ProductDto> deleteChecked(ProductDto productId) {
+        return productMapper.deleteChecked();
     }
 }
