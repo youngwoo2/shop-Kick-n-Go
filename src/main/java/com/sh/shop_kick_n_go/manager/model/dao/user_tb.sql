218f7b96e7d807c0ec844bbfd52923f6bac22951
Tae Jun Park <162237157+tjpark312@users.noreply.github.com>
2024-06-26 09:42:40 +0900

    defualt 사이드 메뉴에 회원 관리 추가 #54 #23
    withdrawalStatus가 Y인경우 삭제 할 수 있는 기능 추가

--- src/main/java/com/sh/shop_kick_n_go/manager/model/dao/user_tb.sql
+++ src/main/java/com/sh/shop_kick_n_go/manager/model/dao/user_tb.sql
@@ -103,3 +103,102 @@ UPDATE user_tb SET user_address = '경상남도 사천시 사천읍 678-89', add
 UPDATE user_tb SET user_address = '서울특별시 종로구 인사동 901-23', address_code = 7 WHERE user_id = 48;
 UPDATE user_tb SET user_address = '경상북도 구미시 형곡동 345-12', address_code = 5 WHERE user_id = 49;
 UPDATE user_tb SET user_address = '서울시 강북구 쌍문동 124-77', address_code = 0 WHERE user_id = 50;
+
+UPDATE user_tb
+SET user_consumption = CASE
+                           WHEN user_grade = '일반회원' THEN FLOOR(RAND() * 200001)
+                           WHEN user_grade = 'vip회원' THEN FLOOR(200000 + RAND() * 300001)
+                           WHEN user_grade = 'vvip회원' THEN FLOOR(500000 + RAND() * 500001)
+    END
+WHERE user_grade IN ('일반회원', 'vip회원', 'vvip회원');
+
+
+UPDATE user_tb SET user_consumption =
+                       CASE
+                           WHEN user_id = 1 THEN 150000
+                           WHEN user_id = 2 THEN 350000
+                           WHEN user_id = 3 THEN 800000
+                           WHEN user_id = 4 THEN 600000
+                           WHEN user_id = 5 THEN 100000
+                           WHEN user_id = 6 THEN 150000
+                           WHEN user_id = 7 THEN 300000
+                           WHEN user_id = 8 THEN 200000
+                           WHEN user_id = 9 THEN 450000
+                           WHEN user_id = 10 THEN 180000
+                           WHEN user_id = 11 THEN 150000
+                           WHEN user_id = 12 THEN 750000
+                           WHEN user_id = 13 THEN 50000
+                           WHEN user_id = 14 THEN 150000
+                           WHEN user_id = 15 THEN 300000
+                           WHEN user_id = 16 THEN 700000
+                           WHEN user_id = 17 THEN 800000
+                           WHEN user_id = 18 THEN 300000
+                           WHEN user_id = 19 THEN 750000
+                           WHEN user_id = 20 THEN 900000
+                           WHEN user_id = 21 THEN 1100000
+                           WHEN user_id = 22 THEN 1300000
+                           WHEN user_id = 23 THEN 100000
+                           WHEN user_id = 24 THEN 250000
+                           WHEN user_id = 25 THEN 400000
+                           WHEN user_id = 26 THEN 20000
+                           WHEN user_id = 27 THEN 50000
+                           WHEN user_id = 28 THEN 1000000
+                           WHEN user_id = 29 THEN 150000
+                           WHEN user_id = 30 THEN 250000
+                           WHEN user_id = 31 THEN 300000
+                           WHEN user_id = 32 THEN 1500000
+                           WHEN user_id = 33 THEN 100000
+                           WHEN user_id = 34 THEN 200000
+                           WHEN user_id = 35 THEN 150000
+                           WHEN user_id = 36 THEN 2000000
+                           WHEN user_id = 37 THEN 1800000
+                           WHEN user_id = 38 THEN 220000
+                           WHEN user_id = 39 THEN 350000
+                           WHEN user_id = 40 THEN 650000
+                           WHEN user_id = 41 THEN 80000
+                           WHEN user_id = 42 THEN 400000
+                           WHEN user_id = 43 THEN 450000
+                           WHEN user_id = 44 THEN 20000
+                           WHEN user_id = 45 THEN 120000
+                           WHEN user_id = 46 THEN 500000
+                           WHEN user_id = 47 THEN 150000
+                           WHEN user_id = 48 THEN 600000
+                           WHEN user_id = 49 THEN 400000
+                           WHEN user_id = 50 THEN 80000
+                           END
+WHERE user_id IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                  31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                  41, 42, 43, 44, 45, 46, 47, 48, 49, 50);
+
+
+UPDATE user_tb
+SET user_consumption = CASE
+                           WHEN user_grade = '일반회원' THEN FLOOR(RAND() * 200001)
+                           WHEN user_grade = 'vip회원' THEN FLOOR(200000 + RAND() * 300001)
+                           WHEN user_grade = 'vvip회원' THEN FLOOR(500000 + RAND() * 500001)
+    END
+WHERE user_grade IN ('일반회원', 'vip회원', 'vvip회원');
+
+
+UPDATE user_tb
+SET user_consumption = CASE
+                           WHEN user_grade = '일반회원' AND user_consumption > 200000 THEN 200000
+                           WHEN user_grade = 'vip회원' AND (user_consumption <= 200000 OR user_consumption > 500000) THEN 500000
+                           WHEN user_grade = 'vvip회원' AND user_consumption <= 500000 THEN 600000
+                           ELSE user_consumption
+    END
+WHERE user_grade IN ('일반회원', 'vip회원', 'vvip회원');
+
+
+UPDATE user_tb
+SET user_consumption = CASE
+                           WHEN user_grade = '일반회원' THEN FLOOR(RAND() * 200001 / 100) * 100
+                           WHEN user_grade = 'vip회원' THEN FLOOR((200000 + RAND() * 300001) / 100) * 100
+                           WHEN user_grade = 'vvip회원' THEN FLOOR((500000 + RAND() * 500001) / 100) * 100
+    END
+WHERE user_grade IN ('일반회원', 'vip회원', 'vvip회원');
+
+select *
+from user_tb;
--- src/main/java/com/sh/shop_kick_n_go/member/model/service/MemberService.java
+++ src/main/java/com/sh/shop_kick_n_go/member/model/service/MemberService.java
@@ -3,12 +3,13 @@ package com.sh.shop_kick_n_go.member.model.service;
 
 import com.sh.shop_kick_n_go.member.model.dao.MemberMapper;
 import com.sh.shop_kick_n_go.member.model.dto.MemberDto;
-import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +18,30 @@ public class MemberService {
     @Autowired
     private MemberMapper memberMapper;
 
-//    public List<MemberDto> findAllUser() {
-//        return memberMapper.findAllUser();
-//    }
-}
+    public List<MemberDto> findAllUser() {
+        return memberMapper.findAllUser();
+    }
 
+
+    public void deleteUserInfoByStatus(List<String> userIds, String withdrawalStatus) {
+        for (String userId : userIds) {
+            Integer userIdInt = Integer.valueOf(userId);
+            MemberDto member = memberMapper.findByUserId(userIdInt);
+
+            if (member == null) {
+                log.error("User with ID {} does not exist.", userId);
+                throw new IllegalArgumentException("User with ID " + userId + " 가 삭제되었습니다.");
+            }
+
+            log.info("Found user: {}", member);
+
+            if (!"Y".equals(member.getWithdrawalStatus())) {
+                log.warn("User with ID {} is not in withdrawal status.", userId);
+                throw new IllegalArgumentException("탈퇴 상태가 아닌 회원은 정보를 삭제할 수 없습니다");
+            }
+
+            memberMapper.deleteUserInfoByStatus(userId, withdrawalStatus);
+        }
+
+    }
+}
